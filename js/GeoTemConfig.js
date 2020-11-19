22d9c3f163119773b17c2680dfca3efc2d319711
Stefan E. Funk <funk@sub.uni-goettingen.de>
2020-11-19 15:04:50 +0100

    Remove trst and pdpdev things

--- js/GeoTemConfig.js
+++ js/GeoTemConfig.js
@@ -57,9 +57,8 @@ GeoTemConfig = {
 	allowColumnRenaming : true,
 	proxy : '/php/proxy.php?address=', // set this if a HTTP proxy shall be used (e.g. to bypass
                                        // X-Domain problems)
-    dariahOwnStorageURL : 'https://cdstar.de.dariah.eu/test/dariah/', // URL of DARIAH-DE OwnStorage
-    // FIXME REMOVE the "/beta" for Release!
-    datasheetEditorURL : '/beta/edit/index.html', // URL of the Datasheet Editor
+    dariahOwnStorageURL : 'https://cdstar.de.dariah.eu/dariah/', // URL of DARIAH-DE OwnStorage
+    datasheetEditorURL : '/edit/index.html', // URL of the Datasheet Editor
     dariahOwnStorageBearerPrefix : 'bearer ',
     dariahOwnStorageLogIDPrefix : 'GEOBRO_',
 	//colors for several datasets; rgb1 will be used for selected objects, rgb0 for unselected
