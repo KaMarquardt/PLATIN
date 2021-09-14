c5642129b891861497dd60e0d3c88e2d36cf5a55
malzer <malzer@sub.uni-goettingen.de>
2021-09-14 13:10:29 +0200

    Change URLs for release

--- js/GeoTemConfig.js
+++ js/GeoTemConfig.js
@@ -58,11 +58,11 @@ GeoTemConfig = {
 	proxy : '/php/proxy.php?address=', // set this if a HTTP proxy shall be used (e.g. to bypass
                                        // X-Domain problems)
     // FIXME Please change for release!
-//    dariahOwnStorageURL : 'https://cdstar.de.dariah.eu/dariah/', // URL of DARIAH-DE OwnStorage
-    dariahOwnStorageURL : 'https://cdstar.de.dariah.eu/test/dariah/', // URL of DARIAH-DE OwnStorage
+    dariahOwnStorageURL : 'https://cdstar.de.dariah.eu/dariah/', // URL of DARIAH-DE OwnStorage
+   // dariahOwnStorageURL : 'https://cdstar.de.dariah.eu/test/dariah/', // URL of DARIAH-DE OwnStorage
     // FIXME Please change for release!
-//    datasheetEditorURL : '/edit/index.html', // URL of the Datasheet Editor
-    datasheetEditorURL : '/beta/edit/index.html', // URL of the Datasheet Editor
+    datasheetEditorURL : '/edit/index.html', // URL of the Datasheet Editor
+   // datasheetEditorURL : '/beta/edit/index.html', // URL of the Datasheet Editor
     dariahOwnStorageBearerPrefix : 'bearer ',
     dariahOwnStorageLogIDPrefix : 'GEOBRO_',
 	//colors for several datasets; rgb1 will be used for selected objects, rgb0 for unselected
